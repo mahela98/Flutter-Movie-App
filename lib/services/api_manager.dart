79593054f5da1772babdf3b3226133e1e3dacea6
mahela98 <chandimaofficial@gmail.com>
2021-09-22 09:07:57 +0530

    created api manager.dart

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,4 +1,29 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../models/movie_data.dart';
+import '../urls/urls.dart';
 
-class apiManager {}
+class apiManager {
+  Future<Movie?> getData() async {
+    var client = http.Client();
+    var movieDataModel;
+
+    try {
+      var url = Uri.parse(MovieDB.moviedb_url);
+      var response = await client.get(url);
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        var jsonString = response.body;
+        // print(jsonString);
+        var jsonMap = await json.decode(jsonString);
+        movieDataModel = Movie.fromJson(jsonMap);
+      }
+    } catch (e) {
+      print('Error _ : $e');
+      return movieDataModel;
+    }
+    return movieDataModel;
+  }
+}
---
+++ lib/urls/urls.dart
@@ -0,0 +1,4 @@
+class MovieDB {
+  static String moviedb_url =
+      'https://api.themoviedb.org/3/trending/movie/week?api_key=ed59011d6c46d7a33abf946104590345';
+}
