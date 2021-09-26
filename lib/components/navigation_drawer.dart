57c11b8b1555da54177104caa1f86b700b7cd306
mahela98 <chandimaofficial@gmail.com>
2021-09-26 11:33:45 +0530

    side drawer modified

--- lib/components/navigation_drawer.dart
+++ lib/components/navigation_drawer.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+import 'package:sizer/sizer.dart';
 import 'package:flutter/material.dart';
 
 class NavigationDrawer extends StatelessWidget {
@@ -5,52 +7,127 @@ class NavigationDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      elevation: 1,
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.black26,
-            ),
-            child: Container(
-              child: Column(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundColor: Colors.transparent,
-                      backgroundImage: NetworkImage(
-                          'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'),
+    return Container(
+      width: 75.w,
+      child: Drawer(
+        elevation: 1,
+        child: Container(
+          decoration: BoxDecoration(color: Colors.blueGrey[900]),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 12,
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: [
+                    DrawerHeader(
+                      padding: EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          colors: [
+                            Colors.amberAccent,
+                            Colors.red,
+                          ],
+                        ),
+                        //color: Colors.amberAccent[700],
+                      ),
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Flexible(
+                              child: CircleAvatar(
+                                radius: 7.h,
+                                backgroundColor: Colors.black,
+                                backgroundImage: NetworkImage(
+                                    'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 2.h,
+                            ),
+                            Text(
+                              'User Name',
+                              style: TextStyle(
+                                  fontSize: 12.sp, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: const Center(
+                            child: Text(
+                          'About',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                        subtitle: Divider(
+                          color: Colors.grey[200],
+                          height: 1,
+                        ),
+                        onTap: () {
+                          // Update the state of the app.
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: const Center(
+                          child: Text(
+                            'Contact',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        subtitle: Divider(
+                          color: Colors.grey[200],
+                          height: 1,
+                        ),
+                        onTap: () {
+                          // Update the state of the app.
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: const Center(
+                            child: Text(
+                          'Help',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                        subtitle: Divider(
+                          color: Colors.grey[200],
+                          height: 1,
+                        ),
+                        onTap: () {
+                          // Update the state of the app.
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  width: 100.h,
+                  color: Colors.amberAccent,
+                  child: Center(
+                    child: Text(
+                      "EACMS Products",
+                      style: TextStyle(
+                          fontSize: 8.sp, fontWeight: FontWeight.w200),
                     ),
                   ),
-                  Text(
-                    'Drawer Header',
-                    textAlign: TextAlign.left,
-                  ),
-                  Text('Drawer Header'),
-                ],
-              ),
-            ),
+                ),
+              )
+            ],
           ),
-          ListTile(
-            title: const Text('Item 1'),
-            onTap: () {
-              // Update the state of the app.
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: const Text('Item 2'),
-            onTap: () {
-              // Update the state of the app.
-              Navigator.pop(context);
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
