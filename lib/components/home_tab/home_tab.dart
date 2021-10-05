ed7f6afc2f645c93b9e7b1f16a28bee1f3b12d7c
mahela98 <chandimaofficial@gmail.com>
2021-10-06 00:25:34 +0530

    -- home page design 03

--- lib/components/home_tab/home_tab.dart
+++ lib/components/home_tab/home_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'dart:ffi';
+import 'package:sizer/sizer.dart';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter_movie_app/components/movie_card.dart';
@@ -27,49 +28,132 @@ class _HomeTabState extends State<HomeTab> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: FutureBuilder<Movie?>(
-                future: _trendingMovieModel,
-                builder: (context, AsyncSnapshot snapshot) {
-                  if (snapshot.data != null) {
-                    return GridView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 5,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        childAspectRatio: 10 / 6,
-                        crossAxisCount: 1,
-                      ),
-                      // scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        var movie = snapshot.data!.results[index];
-                        return Padding(
-                          padding: const EdgeInsets.all(0),
-                          child: TextButton(
-                            child: MovieCard(movie),
-                            onPressed: () {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                return MovieDetailsPage(movie);
-                              }));
-                            },
-                          ),
-                        );
-                      },
-                    );
-                  } else {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                }),
-          ),
-          Expanded(flex: 1, child: Text("asd")),
-        ],
-      ),
+    return ListView(
+      children: [
+        Container(
+            margin: EdgeInsets.fromLTRB(0, 2.h, 0, 1.h),
+            padding: EdgeInsets.fromLTRB(0, 1.2.h, 0, 1.2.h),
+            // color: Colors.black,
+            decoration: const BoxDecoration(
+                backgroundBlendMode: BlendMode.darken,
+                gradient: LinearGradient(
+                  tileMode: TileMode.clamp,
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.indigo,
+                    Colors.black,
+                  ],
+                )),
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(2.w, 0, 0, 0),
+              child: Text(
+                "Top Movies",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 14.sp, letterSpacing: 1.5),
+              ),
+            )),
+        Container(
+          height: 42.h,
+          child: FutureBuilder<Movie?>(
+              future: _trendingMovieModel,
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.data != null) {
+                  return GridView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      childAspectRatio: 9 / 5.5,
+                      crossAxisCount: 1,
+                    ),
+                    // scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      var movie = snapshot.data!.results[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(0),
+                        child: TextButton(
+                          child: MovieCard(movie),
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return MovieDetailsPage(movie);
+                            }));
+                          },
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              }),
+        ),
+        // const Divider(),
+        Container(
+            margin: EdgeInsets.fromLTRB(0, 2.h, 0, 1.h),
+            padding: EdgeInsets.fromLTRB(0, 1.2.h, 0, 1.2.h),
+            // color: Colors.black,
+            decoration: const BoxDecoration(
+                backgroundBlendMode: BlendMode.colorBurn,
+                gradient: LinearGradient(
+                  tileMode: TileMode.clamp,
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  stops: [0.01, 0.2, 0.6],
+                  colors: [
+                    Colors.transparent,
+                    Colors.teal,
+                    Colors.black,
+                  ],
+                )),
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(2.w, 0, 0, 0),
+              child: Text(
+                "Top Tv Shows",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 14.sp, letterSpacing: 1.5),
+              ),
+            )),
+        Container(
+          height: 42.h, // flex: 7,
+          child: FutureBuilder<Movie?>(
+              future: _trendingMovieModel,
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.data != null) {
+                  return GridView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      childAspectRatio: 10 / 6,
+                      crossAxisCount: 1,
+                    ),
+                    // scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      var movie = snapshot.data!.results[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(0),
+                        child: TextButton(
+                          child: MovieCard(movie),
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return MovieDetailsPage(movie);
+                            }));
+                          },
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              }),
+        )
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         title: 'Movie App',
         theme: ThemeData(
           brightness: Brightness.dark,
-          scaffoldBackgroundColor: Colors.blueGrey[900],
+          scaffoldBackgroundColor: Colors.indigo.withOpacity(0.13),
         ),
         initialRoute: '/',
         routes: {
